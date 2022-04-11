c0f21b5250c8d040da9a228853812971fa2395f6
billydean <billy.goehring@gmail.com>
2022-04-11 11:51:57 -0700

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,14 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+
+// POST     /api/users   creates user using information inside request body
+//GET       /api/users      returns users array
+//GET       /api/users/:id  user object with specified id
+//DELETE    /api/users/:id  removes specified user and returns deleted user
+//PUT       /api/users/:id  updates specified user using data from request body. returns modified user
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
